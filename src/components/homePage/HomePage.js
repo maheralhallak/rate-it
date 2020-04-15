dde0146a11b37e9f8e1d31656274e48ab63ef00a
waseem <waseem@waseem.localdomain>
2020-04-15 10:04:32 +0200

    fixing text

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -21,6 +21,13 @@ export default class HomePage extends Component {
           </div>{" "}
           <br />
           <div className="wrapper">
+            <script
+              type="text/javascript"
+              id="hs-script-loader"
+              async
+              defer
+              src="//js.hs-scripts.com/7502045.js"
+            ></script>
             <a className="btn1" href="#">
               <span></span>
             </a>
@@ -78,6 +85,7 @@ export default class HomePage extends Component {
         </div>
       </section>
     );
+
     window.addEventListener("", () => {
       var newone = lr.cloneNode(true);
       lr.parentNode.replaceChild(newone, lr);
