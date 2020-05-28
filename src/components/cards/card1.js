c7adff22ca1c3687ba0c093af5673bddc53fe02a
billo77 <bilalidin77@gmail.com>
2020-05-28 16:17:22 +0200

    items-component

--- src/components/cards/card1.js
+++ src/components/cards/card1.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import './card.css';
-import Category from './Category';
+import './category.css';
 import Links from './Links';
 import data from './Data';
 import { Link } from 'react-router-dom';
@@ -34,16 +34,21 @@ export default function Card1() {
   const renderProducts = currentItems.map((product, index) => {
     return (<div key={product.name} className="col-md-6 mt-5" >
       <Link to={'/item/' + product.id}>
-        <div className="card card2 mt-5" style={{ margin: "0 5px" }}>
-          <container>
-            <Row className="show-grid">
-              <Col>
-                <img className="card1-img1" src={product.image} alt="brand" />
-              </Col>
-            </Row>
-          </container>
-          <h4 className="card1-name d-block text-bold text-center">{product.name}</h4>
-          <div className="description text-center">{product.des}</div>
+        <div className="">
+
+          <div className="col-md-12 card2 mt-2" style={{ margin: "0px 0px" }}>
+            <container>
+              <Row className="show-grid">
+                <Col md={6}>
+                  <img className="card1-img1 d-block mt-3 ml-auto mr-auto" src={product.image} alt="brand" />
+                </Col>
+                <Col md={6}>
+                  <h4 className="card1-name col-md-12 d-block text-bold text-center mt-5">{product.name}</h4>
+                </Col>
+              </Row>
+            </container>
+            <div className="description col-md-12 text-center mt-4">{product.des}</div>
+          </div>
         </div>
       </Link>
     </div>);
@@ -68,17 +73,16 @@ export default function Card1() {
   return (
     <div className="card1">
       <div className="container-fluid mb-5" style={{ padding: 0 }}>
-        <div className="row col-md-12" style={{ padding: 0, margin: "0 10px" }}>
+        <div className="row col-md-12" style={{ padding: 0, margin: "0 0px" }}>
 
-          <Category uri='item' />
+          <h1 className="card1-h1 text-center ml-auto mr-auto mt-3">Check the latest from our brands</h1>
 
-
-          <div className="row col-md-8">
+          <div className="row col-md-12">
             {/* First Card  */}
             {
               renderProducts
             }
-            <ul style={{ marginTop: "50px" }} id="page-numbers">
+            <ul className="ml-auto mr-auto" style={{ marginTop: "80px" }} id="page-numbers">
               {renderPageNumbers}
             </ul>
           </div>
